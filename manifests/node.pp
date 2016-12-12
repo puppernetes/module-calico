3cf228c1a983507c2d6038fd4f3f81e107ccdd27
Sean Turner <sean.turner@jetstack.io>
2016-12-12 14:50:19 +0000

    Linted and validated

--- manifests/node.pp
+++ manifests/node.pp
@@ -7,33 +7,33 @@ define calico::node (
   include ::systemd
   include k8s
 
-  wget::fetch { "calicoctl-v${calico_cni_version}":
-    source => "https://github.com/projectcalico/calico-containers/releases/download/v${calico_node_version}/calicoctl",
+  wget::fetch { "calicoctl-v${calico_node_version}":
+    source      => "https://github.com/projectcalico/calico-containers/releases/download/v${calico_node_version}/calicoctl",
     destination => '/opt/cni/bin/',
-    require => Class['calico'],
-    before => File["/opt/cni/bin/calicoctl"],
+    require     => Class['calico'],
+    before      => File['/opt/cni/bin/calicoctl'],
   }
 
-  file { ["/opt/cni/bin/calicoctl"]:
+  file { ['/opt/cni/bin/calicoctl']:
     ensure => file,
     mode   => '0755',
   }
 
-  file { "/etc/calico/calico.env":
-    ensure => file,
+  file { '/etc/calico/calico.env':
+    ensure  => file,
     content => template('calico/calico.env.erb'),
     require => Class['calico'],
   }
 
-  file { "/usr/lib/systemd/system/calico-node.service":
-    ensure => file,
+  file { '/usr/lib/systemd/system/calico-node.service':
+    ensure  => file,
     content => template('calico/calico-node.service.erb'),
   } ~>
   Exec['systemctl-daemon-reload']
 
-  service { "calico-node":
-    ensure => running,
-    enable => true,
-    require => [ Class["k8s"], File["/etc/calico/calico.env"], File["/usr/lib/systemd/system/calico-node.service"], Exec["Trigger etcd overlay cert"] ],
+  service { 'calico-node':
+    ensure  => running,
+    enable  => true,
+    require => [ Class['k8s'], File['/etc/calico/calico.env'], File['/usr/lib/systemd/system/calico-node.service'], Exec['Trigger etcd overlay cert'] ],
   }
 }
